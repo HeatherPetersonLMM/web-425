47beb5d3293b93d174f4462c4a568bd70ce259a4
Heather <hpeterson@my.bellevue.edu>
2018-07-21 14:23:49 -0400

    adding assignment 1.5

---
+++ week-1/peterson-hello-modules/src/app/shipping/shipping.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShippingComponent } from './shipping.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShippingComponent]
+})
+export class ShippingModule { }
