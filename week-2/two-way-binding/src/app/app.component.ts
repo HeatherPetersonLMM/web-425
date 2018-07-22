21ed7e1ba0ab0eba1f1350a13fdfcac8ec8fb9ba
Heather <hpeterson@my.bellevue.edu>
2018-07-22 15:24:29 -0400

    adding buttons

--- week-2/two-way-binding/src/app/app.component.ts
+++ week-2/two-way-binding/src/app/app.component.ts
@@ -2,8 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <br /><br /><br />
+
+  <div class="container-fluid two-way-form">
+    <h2>Two-Way Binding Example</h2><br />
+    <div class="form-group">
+        <input class="form-control" type="text",
+               placeholder="Heather Peterson"
+               [(ngModel)]="name">
+        <button class="btn btn-primary form-control" (click)="name=''">Clear Name</button>
+    </div>
+    <div class="form-group">
+        <p class="alert-success">Welcome to two-way binding, {{name}}! </p>
+    </div>
+  </div>
+`,
+styles: [
+  `
+    .two-way-form {
+      width: 50%,
+    }
+  `
+]
 })
 export class AppComponent {
   title = 'app';
