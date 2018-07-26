a32c18c84fb1ed8c3cfc2fe8e0915c23523887f8
Heather <hpeterson@my.bellevue.edu>
2018-07-26 15:17:00 -0400

    adding components

--- week-3/peterson-query-params/src/app/app.module.ts
+++ week-3/peterson-query-params/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { E404Component } from './e404/e404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
---
+++ week-3/peterson-query-params/src/app/e404/e404.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e404',
+  template: `
+    <p>
+      e404 works!
+    </p>
+  `,
+  styles: []
+})
+export class E404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
