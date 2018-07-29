dfb1588d2109ea7a1bbc7b0d2d6c430caf64d722
Heather <hpeterson@my.bellevue.edu>
2018-07-29 13:26:02 -0400

    editing home.component

--- week-3/peterson-query-params/src/app/home/home.component.ts
+++ week-3/peterson-query-params/src/app/home/home.component.ts
@@ -3,17 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="container">
+      <div class="row">
+        <h2>Welcome to the Home Page!</h2>
+      </div>
+    </div>
   `,
-  styles: []
+  styles: [ `
+    .container {
+      margin-top: 20px
+    }
+    h2 {
+      color: steelblue;
+    }
+  `
+  ]
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-}
+  }}
