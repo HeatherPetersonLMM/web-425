fca7993885586cdea578a5e62d55ea3622d954ee
Heather <hpeterson@my.bellevue.edu>
2018-07-29 13:24:08 -0400

    editing e404.component

--- week-3/peterson-query-params/src/app/e404/e404.component.ts
+++ week-3/peterson-query-params/src/app/e404/e404.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-e404',
   template: `
-    <p>
-      e404 works!
-    </p>
+    <div class="container">
+      <div class="row">
+        <h2>Page Not Found!</h2>
+      </div>
+    </div>
   `,
-  styles: []
+  styles: [`
+    .container {
+      margin-top: 20px
+    }
+    h2 {
+      color: red;
+    }
+  `]
 })
 export class E404Component implements OnInit {
 
