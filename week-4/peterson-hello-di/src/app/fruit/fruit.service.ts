957eb4d4f2a8b83186eaeaec65d62065fa6ac28d
Heather <hpeterson@my.bellevue.edu>
2018-08-05 11:00:42 -0400

    adding injectable fruit

--- week-4/peterson-hello-di/src/app/fruit/fruit.service.ts
+++ week-4/peterson-hello-di/src/app/fruit/fruit.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Fruit } from './fruit';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FruitService {
-
+  getFruit(): Fruit {
+    return new Fruit(2011, 'Yellow', 'Bananas', 'Giant Eagle, Pittsburgh PA');
+  }
   constructor() { }
 }
