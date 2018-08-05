c6128159c58cd88f1c88f345cc2f6c5a0a5a5d27
Heather <hpeterson@my.bellevue.edu>
2018-08-05 11:06:40 -0400

    adding provider

--- week-4/peterson-hello-di/src/app/fruit/fruit.component.ts
+++ week-4/peterson-hello-di/src/app/fruit/fruit.component.ts
@@ -1,9 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FruitService } from './fruit.service';
 
 @Component({
   selector: 'app-fruit',
-  templateUrl: './fruit.component.html',
-  styleUrls: ['./fruit.component.css']
+  template: `
+  <div class="container">
+      <div class="row">
+        <h2>User Details</h2>
+        <table class="table table-hover table-striped">
+          <thead class="tbl-header">
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ fruit.id }}</td>
+              <td>{{ fruit.name }}</td>
+              <td>{{ fruit.color }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .tbl-header {
+      background-color: steelblue;
+      color: white;
+    }
+  `],
+  providers: [FruitService]
 })
 export class FruitComponent implements OnInit {
 
