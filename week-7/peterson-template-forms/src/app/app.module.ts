188ba93e2265274695ee4d88630421382e4feb82
Heather <hpeterson@my.bellevue.edu>
2018-08-26 17:06:59 -0400

    adding login

--- week-7/peterson-template-forms/src/app/app.module.ts
+++ week-7/peterson-template-forms/src/app/app.module.ts
@@ -1,11 +1,24 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {
+  MatButtonModule,
+  MatListModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -14,14 +27,19 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
     MatMenuModule,
-    MatToolbarModule,
     FlexLayoutModule,
+    FormsModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
