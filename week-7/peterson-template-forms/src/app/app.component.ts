d881fcc16dd39bd7e0e84e2c23d906351c2d9d60
Heather <hpeterson@my.bellevue.edu>
2018-08-26 17:31:55 -0400

    finalizing 7.2

--- week-7/peterson-template-forms/src/app/app.component.ts
+++ week-7/peterson-template-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peterson-template-forms';
+  title = 'app';
+
+  onSubmit(formData) {
+    console.log(formData);
+  }
 }
