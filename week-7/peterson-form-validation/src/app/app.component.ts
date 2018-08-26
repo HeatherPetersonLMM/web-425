914e84258d34cca9ca06858527106052764fede1
Heather <hpeterson@my.bellevue.edu>
2018-08-26 16:43:09 -0400

    editing/adding exercise 7.3 - running nav bar

--- week-7/peterson-form-validation/src/app/app.component.ts
+++ week-7/peterson-form-validation/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-
-  onSubmit(formData) {
-    console.log(formData);
-  }
+  title = 'peterson-form-validation';
 }
