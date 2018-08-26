99589a4c3bac1db4a5b2b484bde42284272e122d
Heather <hpeterson@my.bellevue.edu>
2018-08-26 12:49:55 -0400

    editing

--- week-7/peterson-form-validation/src/app/app.component.ts
+++ week-7/peterson-form-validation/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peterson-form-validation';
+  title = 'app';
+
+  onSubmit(formData) {
+    console.log(formData);
+  }
 }
